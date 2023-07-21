b9388a5ccebcfd1fe7b7f2e908a6e8463fef8995
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-07-21 15:04:45 +0100

    Added fate redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  redirects: {
+    "/fate": "https://aosasona.github.io/fate/setup.sh",
+  },
 });
