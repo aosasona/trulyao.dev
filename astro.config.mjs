8496202b012d11dcd699bd9b3d1e3df9e9191e98
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-04-03 06:57:26 +0100

    Added MDX support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,33 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import solidJs from "@astrojs/solid-js";
-
 import gruvboxMaterialTheme from "./src/themes/gruvbox-material-dark.json";
 import externalize from "./src/plugins/externalize";
 
+import mdx from "@astrojs/mdx";
+
+// TODO: remove plugins
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), solidJs()],
-  markdown: {
-    shikiConfig: {
-      theme: gruvboxMaterialTheme,
-    },
-    rehypePlugins: [[externalize, { domain: "trulyao.dev" }]],
-  },
-  redirects: {
-    "/live": {
-      status: 302,
-      destination: "https://twitch.tv/iamtrulyao",
-    },
-  },
+	integrations: [tailwind(), icon(), solidJs(), mdx()],
+	markdown: {
+		shikiConfig: {
+			theme: gruvboxMaterialTheme,
+		},
+		rehypePlugins: [
+			[
+				externalize,
+				{
+					domain: "trulyao.dev",
+				},
+			],
+		],
+	},
+	redirects: {
+		"/live": {
+			status: 302,
+			destination: "https://twitch.tv/iamtrulyao",
+		},
+	},
 });
