1a51228e38d91b464a67102a7fff6e4707543a5d
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-04-10 06:14:43 +0100

    Formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,29 +11,29 @@ import mdx from "@astrojs/mdx";
 // TODO: remove plugins
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), icon(), solidJs(), mdx()],
-	markdown: {
-		shikiConfig: {
-			themes: {
-				light: gruvboxMaterialLight,
-				dark: gruvboxMaterialDark,
-			},
-			defaultColor: false,
-			wrap: true,
-		},
-		rehypePlugins: [
-			[
-				externalize,
-				{
-					domain: "trulyao.dev",
-				},
-			],
-		],
-	},
-	redirects: {
-		"/live": {
-			status: 302,
-			destination: "https://twitch.tv/iamtrulyao",
-		},
-	},
+  integrations: [tailwind(), icon(), solidJs(), mdx()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: gruvboxMaterialLight,
+        dark: gruvboxMaterialDark,
+      },
+      defaultColor: false,
+      wrap: true,
+    },
+    rehypePlugins: [
+      [
+        externalize,
+        {
+          domain: "trulyao.dev",
+        },
+      ],
+    ],
+  },
+  redirects: {
+    "/live": {
+      status: 302,
+      destination: "https://twitch.tv/iamtrulyao",
+    },
+  },
 });
