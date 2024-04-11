2da6ceb437551f8ee7a2f70167f5742c7af5136c
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-04-11 17:31:49 +0100

    Added more projects

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,38 +1,40 @@
 import { z, defineCollection } from "astro:content";
 
 const blogCollection = defineCollection({
-	type: "content",
-	schema: z.object({
-		title: z.string(),
-		tags: z.array(z.string()).default([]).optional(),
-		date: z.string().transform((str) => new Date(str)),
-		description: z.string(),
-		keywords: z.string(),
-		draft: z.boolean().default(false),
-	}),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()).default([]).optional(),
+    date: z.string().transform((str: string) => new Date(str)),
+    description: z.string(),
+    keywords: z.string(),
+    draft: z.boolean().default(false),
+  }),
 });
 
 const projectCollection = defineCollection({
-	type: "data",
-	schema: z.array(
-		z.object({
-			name: z.string().regex(/^[A-Za-z0-9-\.\s]+$/),
-			description: z.string(),
-			website_url: z.string().url().optional().nullable(),
-			github_url: z.string().url().optional().nullable(),
-			image: z.string().regex(/\.(png|jpg|jpeg|gif)$/),
-			status: z.enum([
-				"active",
-				"unmaintained",
-				"archived",
-				"private",
-				"deprecated",
-			]),
-		}),
-	),
+  type: "data",
+  schema: z.array(
+    z.object({
+      name: z.string().regex(/^[A-Za-z0-9-\.\s]+$/),
+      description: z.string(),
+      website_url: z.string().url().optional().nullable(),
+      github_url: z.string().url().optional().nullable(),
+      image: z.string().regex(/\.(png|jpg|jpeg|gif)$/),
+      tags: z.array(z.string().optional().nullable()),
+      status: z.enum([
+        "active",
+        "unmaintained",
+        "archived",
+        "private",
+        "deprecated",
+        "wip"
+      ]),
+    }),
+  ),
 });
 
 export const collections = {
-	blog: blogCollection,
-	projects: projectCollection,
+  blog: blogCollection,
+  projects: projectCollection,
 };
