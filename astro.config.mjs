e6acddc4f5ce9c154252e5577c30cb6a21fb972c
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-04-07 17:36:27 +0100

    Update twitch URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,13 +9,13 @@ import mdx from "@astrojs/mdx";
 // TODO: remove plugins
 // https://astro.build/config
 export default defineConfig({
-  site: "https://trulyao.dev",
-  integrations: [tailwind(), icon(), solidJs(), mdx()],
-  markdown: {
-    syntaxHighlight: "prism",
-    rehypePlugins: [[externalize, { domain: "trulyao.dev" }]],
-  },
-  redirects: {
-    "/live": { status: 302, destination: "https://twitch.tv/truly_ao" },
-  },
+	site: "https://trulyao.dev",
+	integrations: [tailwind(), icon(), solidJs(), mdx()],
+	markdown: {
+		syntaxHighlight: "prism",
+		rehypePlugins: [[externalize, { domain: "trulyao.dev" }]],
+	},
+	redirects: {
+		"/live": { status: 302, destination: "https://twitch.tv/3x3cv3" },
+	},
 });
