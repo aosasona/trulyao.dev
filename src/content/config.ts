b2d0fa079d1d03932f72ffd77c1b29d9ca3e6498
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-08-17 11:52:12 +0100

    Add demo to project status and format file

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,53 +1,54 @@
 import { z, defineCollection } from "astro:content";
 
 const blogCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    tags: z.array(z.string()).default([]).optional(),
-    date: z.date(),
-    description: z.string(),
-    keywords: z.string(),
-    draft: z.boolean().default(false),
-  }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		tags: z.array(z.string()).default([]).optional(),
+		date: z.date(),
+		description: z.string(),
+		keywords: z.string(),
+		draft: z.boolean().default(false),
+	}),
 });
 
 const notesCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    tags: z.array(z.string()).default([]).optional(),
-    date: z.date(),
-    description: z.string().optional(),
-    keywords: z.string(),
-    draft: z.boolean().default(true),
-  }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		tags: z.array(z.string()).default([]).optional(),
+		date: z.date(),
+		description: z.string().optional(),
+		keywords: z.string(),
+		draft: z.boolean().default(true),
+	}),
 });
 
 const projectCollection = defineCollection({
-  type: "data",
-  schema: z.array(
-    z.object({
-      name: z.string().regex(/^[A-Za-z0-9-\.\s]+$/),
-      description: z.string(),
-      website_url: z.string().url().optional().nullable(),
-      github_url: z.string().url().optional().nullable(),
-      image: z.string().regex(/\.(png|jpg|jpeg|gif)$/),
-      tags: z.array(z.string().optional()),
-      status: z.enum([
-        "active",
-        "unmaintained",
-        "archived",
-        "private",
-        "deprecated",
-        "wip"
-      ]),
-    }),
-  ),
+	type: "data",
+	schema: z.array(
+		z.object({
+			name: z.string().regex(/^[A-Za-z0-9-\.\s]+$/),
+			description: z.string(),
+			website_url: z.string().url().optional().nullable(),
+			github_url: z.string().url().optional().nullable(),
+			image: z.string().regex(/\.(png|jpg|jpeg|gif)$/),
+			tags: z.array(z.string().optional()),
+			status: z.enum([
+				"active",
+				"unmaintained",
+				"archived",
+				"private",
+				"deprecated",
+				"wip",
+				"demo",
+			]),
+		}),
+	),
 });
 
 export const collections = {
-  blog: blogCollection,
-  projects: projectCollection,
-  notes: notesCollection,
+	blog: blogCollection,
+	projects: projectCollection,
+	notes: notesCollection,
 };
